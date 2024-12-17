808482e6d63bdaa7335495ba9c2a658019debf10
Mehran Shahidi <m.mehrashahidi@gmail.com>
2024-12-17 16:31:54 +0100

    ✨ feat: add LH example code

--- lh-plugin-demo/src/Demo/Lib.hs
+++ lh-plugin-demo/src/Demo/Lib.hs
@@ -2,23 +2,8 @@
 
 module Demo.Lib where
 
-import Data.Set
-
-{-@ type Pos = {v:Int | 0 < v} @-}
-
-{-@ incr :: Pos -> Pos @-}
-incr :: Int -> Int
-incr x = x + 1
-
-{-@ measure elts @-}
-elts :: (Ord a) => [a] -> Set a
-elts [] = empty
-elts (x : xs) = singleton x `union` elts xs
-
-{-@ rev :: xs:_ -> {v:_ | elts v == elts xs} @-}
-rev :: [a] -> [a]
-rev = go []
- where
-  {-@ go :: acc:_ -> xs:_ -> {v:_ | elts v == union (elts acc) (elts xs)} @-}
-  go acc [] = acc
-  go acc (x : xs) = go (x : acc) xs
+-- {-@ reflect smartInsert @-}
+-- smartInsert :: String -> Int -> [(String, Int)] -> [(String, Int)]
+-- smartInsert k v l
+--   | lookup k l == Just v = l
+--   | otherwise = (k, v) : l
